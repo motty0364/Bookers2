656bafd450ac3972c82bc0121fc4c30e2c7f1f89
YuYamamoto <115370416+motty0364@users.noreply.github.com>
2024-03-31 19:43:35 +0000

    完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,37 @@
 class UsersController < ApplicationController
-  def edit
-  end
-
+  
   def index
+    @book = Book.new
+    @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
+    @book = Book.new
+    @books = @user.books
+  end
+  
+  def edit
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to user_path(current_user.id)
+    end
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
